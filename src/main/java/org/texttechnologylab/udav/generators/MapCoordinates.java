5204250144317478c7ba1a47cd67e507b968dab1
julian <s1170492@stud.uni-frankfurt.de>
2025-11-30 19:55:05 +0100

    Setup MapCoordinates generator

--- src/main/java/org/texttechnologylab/udav/generators/MapCoordinates.java
+++ src/main/java/org/texttechnologylab/udav/generators/MapCoordinates.java
@@ -8,6 +8,7 @@ import org.texttechnologylab.udav.sources.DBAccess;
 
 import java.awt.*;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -21,11 +22,65 @@ public class MapCoordinates extends Generator {
 
     @Override
     public void setup() throws SQLException {
+        entries = new ArrayList<>();
         if (SourceJson.class.equals(source.getClass())) {
+            SourceJson sourceJson = (SourceJson) source;
+
+            Map<String, Object> keysMap = (Map<String, Object>) settings.getMapSettingOrDefault("keysMap", null);
+            List<Map<String, Object>> flatKeysMap = sourceJson.generateFlatKeysMap(keysMap);
+            for (Map<String, Object> map : flatKeysMap) {
+
+                // Coordinates (mandatory field)
+                Map<String, Number> coordinates = (Map<String, Number>) map.get("coordinates");
+                if (coordinates == null) continue;
+                ArrayList<Number> coordinatesNumbers = new ArrayList<>();
+                for (int c = 0; true; c++) {
+                    String coordinateString = Integer.toString(c);
+                    Number coordinateNumber = coordinates.get(coordinateString);
+                    if (coordinateNumber == null) break;
+                    coordinatesNumbers.add(coordinateNumber);
+                }
+                if (coordinatesNumbers.isEmpty()) continue;
+
+                // Label
+                String label = (String) map.get("label");
+
+                // Scale
+                Number scale = (Number) map.get("scale");
+                if (scale == null) scale = 1.0;
+
+                // FillColor
+                Color fillColor = mapRGBAorStringToColor(map.get("fillColor"), Color.BLUE);
+
+                // StrokeColor
+                Color strokeColor = mapRGBAorStringToColor(map.get("strokeColor"), Color.RED);
+
+                // OutsideColor
+                Color outsideColor = Color.WHITE; // TODO: Custom color
+
+                entries.add(new Entry(sourceJson.getSingleFileName(), label, coordinatesNumbers, scale, fillColor, strokeColor, outsideColor));
+                // TODO: Allow multiple file sources for JSON
+            }
             System.out.println("test!");
-            Map<?, ?> keysMap = settings.getMapSettingOrDefault("keysMap", null);
-            SourceJson.generateFlatKeysMap(null);
         }
+    }
+
+    private Color mapRGBAorStringToColor(Object colorObj, Color defaultColor) {
+        try {
+            if (colorObj == null) {
+                return defaultColor;
+            } else if (String.class.equals(colorObj.getClass())) {
+                return Color.decode((String) colorObj);
+            } else if (colorObj instanceof Map<?,?>) {
+                Map<String, Number> colorObjMap = (Map<String, Number>) colorObj;
+                Number red = colorObjMap.getOrDefault("Red", 0.0);
+                Number green = colorObjMap.getOrDefault("Green", 0.0);
+                Number blue = colorObjMap.getOrDefault("Blue", 0.0);
+                Number alpha = colorObjMap.getOrDefault("Alpha", 0.0);
+                return new Color(red.floatValue(), green.floatValue(), blue.floatValue(), alpha.floatValue());
+            }
+        } catch (Exception ignored) {}
+        return defaultColor;
     }
 
     @Override
@@ -34,14 +89,16 @@ public class MapCoordinates extends Generator {
     }
 
     private static class Entry {
+        private final String filename;
         private final String label;
-        private final double[] coordinates;
-        private final double scale;
+        private final List<Number> coordinates;
+        private final Number scale;
         private final Color fillColor;
         private final Color strokeColor;
         private final Color outsideColor;
 
-        private Entry(String label, double[] coordinates, double scale, Color fillColor, Color strokeColor, Color outsideColor) {
+        private Entry(String filename, String label, List<Number> coordinates, Number scale, Color fillColor, Color strokeColor, Color outsideColor) {
+            this.filename = filename;
             this.label = label;
             this.coordinates = coordinates;
             this.scale = scale;
--- src/main/java/org/texttechnologylab/udav/generators/sources/SourceJson.java
+++ src/main/java/org/texttechnologylab/udav/generators/sources/SourceJson.java
@@ -3,13 +3,17 @@ package org.texttechnologylab.udav.generators.sources;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.Getter;
 import org.texttechnologylab.udav.pipeline.JSONView;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+@Getter
 public class SourceJson extends Source {
 
     public static final String SOURCE_FILES_PATH = "sourcefilesJSON";
@@ -29,8 +33,92 @@ public class SourceJson extends Source {
         this.filenameToJsonView = null;
     }
 
-    public static List<Map<String, Object>> generateFlatKeysMap(Map<String, Object> keysMapRoot) {
-        return null;
+    public List<Map<String, Object>> generateFlatKeysMap(Map<String, Object> keysMapRoot) {
+        ArrayList<Map<String, Object>> flatKeysMap = new ArrayList<>();
+        if (keysMapRoot == null) return flatKeysMap;
+        generateFlatKeysMapRecursive(keysMapRoot, singleFileJSONView, flatKeysMap);
+        return flatKeysMap;
+    }
+    private void generateFlatKeysMapRecursive(Map<String, Object> keysMapCurrentPosition, JSONView currentPosition, ArrayList<Map<String, Object>> flatKeysMap) {
+        for (Map.Entry<String, Object> entry : keysMapCurrentPosition.entrySet()) {
+            String key = entry.getKey();
+            Object value = entry.getValue();
+            if (String.class.equals(value.getClass())) {
+                List valueNode = (List) currentPosition.get(key).getNode();
+                String stringValue = ((String) value).trim();
+                int valueNodeSize = valueNode.size();
+                if (stringValue.contains("@")) {
+                    String[] split = stringValue.split("@");
+                    for (int i = 0; i < valueNodeSize; i++) {
+                        Map<String, Object> innerMap;
+                        if (flatKeysMap.size() < i + 1) {
+                            innerMap = new HashMap<>();
+                            flatKeysMap.add(innerMap);
+                        } else {
+                            innerMap = flatKeysMap.get(i);
+                        }
+                        Map<String, Object> innerValueMap = (Map<String, Object>) innerMap.get(split[0]);
+                        if (innerValueMap == null) {
+                            innerValueMap = new HashMap<>();
+                            innerMap.put(split[0], innerValueMap);
+                        }
+                        innerValueMap.put(split[1], valueNode.get(i));
+                    }
+                } else {
+                    for (int i = 0; i < valueNodeSize; i++) {
+                        Map<String, Object> innerMap;
+                        if (flatKeysMap.size() < i + 1) {
+                            innerMap = new HashMap<>();
+                            flatKeysMap.add(innerMap);
+                        } else {
+                            innerMap = flatKeysMap.get(i);
+                        }
+                        innerMap.put(stringValue, valueNode.get(i));
+                    }
+                }
+            } else if (value instanceof List) {
+                List valueNode = (List) currentPosition.get(key).getNode();
+                List listValue = (List) value;
+                int valueNodeSize = valueNode.size();
+                int listSize = listValue.size();
+                for (int listIndex = 0; listIndex < listSize; listIndex++) {
+                    String stringValue = (String) listValue.get(listIndex);
+                    if (stringValue.contains("@")) {
+                        String[] split = stringValue.split("@");
+                        for (int i = 0; i < valueNodeSize; i++) {
+                            Map<String, Object> innerMap;
+                            if (flatKeysMap.size() < i + 1) {
+                                innerMap = new HashMap<>();
+                                flatKeysMap.add(innerMap);
+                            } else {
+                                innerMap = flatKeysMap.get(i);
+                            }
+                            Map<String, Object> innerValueMap = (Map<String, Object>) innerMap.get(split[0]);
+                            if (innerValueMap == null) {
+                                innerValueMap = new HashMap<>();
+                                innerMap.put(split[0], innerValueMap);
+                            }
+                            List subListValueNode = (List) valueNode.get(i);
+                            innerValueMap.put(split[1], subListValueNode.get(listIndex));
+                        }
+                    } else {
+                        for (int i = 0; i < valueNodeSize; i++) {
+                            Map<String, Object> innerMap;
+                            if (flatKeysMap.size() < i + 1) {
+                                innerMap = new HashMap<>();
+                                flatKeysMap.add(innerMap);
+                            } else {
+                                innerMap = flatKeysMap.get(i);
+                            }
+                            List subListValueNode = (List) valueNode.get(i);
+                            innerMap.put(stringValue, subListValueNode.get(listIndex));
+                        }
+                    }
+                }
+            } else if (value instanceof Map) {
+                generateFlatKeysMapRecursive((Map<String, Object>) value, currentPosition.get(key), flatKeysMap);
+            }
+        }
     }
 
     private static JSONView readJsonViewFromFile(String path) throws IOException {
