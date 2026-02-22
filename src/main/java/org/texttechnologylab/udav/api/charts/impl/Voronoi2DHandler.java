469c76930bb744dd7d1d0a9d33669614de44c037
julian <s1170492@stud.uni-frankfurt.de>
2026-02-22 12:42:50 +0100

    added VoronoiDiagram API

--- src/main/java/org/texttechnologylab/udav/api/charts/impl/Voronoi2DHandler.java
+++ src/main/java/org/texttechnologylab/udav/api/charts/impl/Voronoi2DHandler.java
@@ -12,7 +12,7 @@ import org.texttechnologylab.udav.api.charts.ChartHandler;
 import java.util.*;
 import java.util.stream.Collectors;
 
-@Component("Voronoi")
+@Component("VoronoiDiagram")
 @RequiredArgsConstructor
 public class Voronoi2DHandler implements ChartHandler {
 
@@ -26,22 +26,34 @@ public class Voronoi2DHandler implements ChartHandler {
                            Set<String> files,
                            ValueMode valueMode,
                            String schema) {
-
-        // Which fields to emit (default: label,value,color)
-        LinkedHashSet<String> attrs = ChartHandler.parseCsvSet(filters.getOrDefault("attrs", "label,value,color"))
-                .stream().collect(Collectors.toCollection(LinkedHashSet::new));
-        if (attrs.isEmpty()) attrs = new LinkedHashSet<>(List.of("label", "value", "color"));
-
-        Map<String, List<GeneratorDataRepository.MapCoordinatesRow>> data = repo.loadMapCoordinatesByFile(schema, generatorId);
-
         ArrayNode out = mapper.createArrayNode();
-        for (List<GeneratorDataRepository.MapCoordinatesRow> list : data.values()) {
-            for (GeneratorDataRepository.MapCoordinatesRow d : list) {
-
-            }
-        }
-
-
-        return null;
+        var o1 = mapper.createObjectNode();
+        o1.put("label", "Cell 1");
+        o1.put("x", -0.17228836433487893);
+        o1.put("y", 0.0004034504818264395);
+        o1.put("fill", "#FF5400FF");
+        o1.put("stroke", "#800000FF");
+        o1.put("scale", 0.007084618021265124);
+        o1.put("abs", 0.008337255160062937);
+        out.add(o1);
+        var o2 = mapper.createObjectNode();
+        o2.put("label", "Cell 2");
+        o2.put("x", 0.08807457534091101);
+        o2.put("y", 0.288377970457077);
+        o2.put("fill", "#FFAA00FF");
+        o2.put("stroke", "#FF9090FF");
+        o2.put("scale", 0.41333009767848083);
+        o2.put("abs", 0.1593699070893901);
+        out.add(o2);
+        var o3 = mapper.createObjectNode();
+        o3.put("label", "Cell 3");
+        o3.put("x", -0.31771938753358686);
+        o3.put("y", 0.0005067524034529924);
+        o3.put("fill", "#FEFF00FF");
+        o3.put("stroke", "#800000FF");
+        o3.put("scale", 0.018789279751973822);
+        o3.put("abs", 0.012688777059128192);
+        out.add(o3);
+        return out;
     }
 }
