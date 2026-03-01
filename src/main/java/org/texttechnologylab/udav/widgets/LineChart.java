e157f276943b54251aa5bdb40df0d4bc586c89bb
julian <s1170492@stud.uni-frankfurt.de>
2026-03-01 15:26:49 +0100

    add linechart dummy data

--- src/main/java/org/texttechnologylab/udav/widgets/LineChart.java
+++ src/main/java/org/texttechnologylab/udav/widgets/LineChart.java
@@ -24,33 +24,32 @@ public class LineChart extends Widget {
                            ValueMode valueMode,
                            String schema) {
         ArrayNode out = mapper.createArrayNode();
-        var o1 = mapper.createObjectNode();
-        o1.put("label", "Cell 1");
-        o1.put("x", -0.17228836433487893);
-        o1.put("y", 0.0004034504818264395);
-        o1.put("fill", "#FF5400FF");
-        o1.put("stroke", "#800000FF");
-        o1.put("scale", 0.007084618021265124);
-        o1.put("abs", 0.008337255160062937);
-        out.add(o1);
-        var o2 = mapper.createObjectNode();
-        o2.put("label", "Cell 2");
-        o2.put("x", 0.08807457534091101);
-        o2.put("y", 0.288377970457077);
-        o2.put("fill", "#FFAA00FF");
-        o2.put("stroke", "#FF9090FF");
-        o2.put("scale", 0.41333009767848083);
-        o2.put("abs", 0.1593699070893901);
-        out.add(o2);
-        var o3 = mapper.createObjectNode();
-        o3.put("label", "Cell 3");
-        o3.put("x", -0.31771938753358686);
-        o3.put("y", 0.0005067524034529924);
-        o3.put("fill", "#FEFF00FF");
-        o3.put("stroke", "#800000FF");
-        o3.put("scale", 0.018789279751973822);
-        o3.put("abs", 0.012688777059128192);
-        out.add(o3);
+        // First dataset
+        var dataset1 = mapper.createObjectNode();
+        dataset1.put("name", "Dataset Test");
+        dataset1.put("color", "#00618f");
+        ArrayNode coordinates1 = mapper.createArrayNode();
+        coordinates1.add(mapper.createObjectNode().put("y", 5).put("x", 0));
+        coordinates1.add(mapper.createObjectNode().put("y", 20).put("x", 20));
+        coordinates1.add(mapper.createObjectNode().put("y", 10).put("x", 40));
+        coordinates1.add(mapper.createObjectNode().put("y", 40).put("x", 60));
+        coordinates1.add(mapper.createObjectNode().put("y", 5).put("x", 80));
+        coordinates1.add(mapper.createObjectNode().put("y", 60).put("x", 100));
+        dataset1.set("coordinates", coordinates1);
+        out.add(dataset1);
+        // Second dataset
+        var dataset2 = mapper.createObjectNode();
+        dataset2.put("name", "Test 2");
+        dataset2.put("color", "#ff8000");
+        ArrayNode coordinates2 = mapper.createArrayNode();
+        coordinates2.add(mapper.createObjectNode().put("y", 10).put("x", 0));
+        coordinates2.add(mapper.createObjectNode().put("y", 15).put("x", 20));
+        coordinates2.add(mapper.createObjectNode().put("y", 30).put("x", 40));
+        coordinates2.add(mapper.createObjectNode().put("y", 25).put("x", 60));
+        coordinates2.add(mapper.createObjectNode().put("y", 50).put("x", 80));
+        coordinates2.add(mapper.createObjectNode().put("y", 35).put("x", 100));
+        dataset2.set("coordinates", coordinates2);
+        out.add(dataset2);
         return out;
     }
 }
