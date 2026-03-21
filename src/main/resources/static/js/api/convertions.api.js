018ba6cd061f4e119189d65b7916ff2fcb089fc4
phpp28 <pstephan99@web.de>
2026-03-21 20:34:57 +0100

    Use API for csv exports

--- src/main/resources/static/js/api/convertions.api.js
+++ src/main/resources/static/js/api/convertions.api.js
@@ -3,3 +3,7 @@ import { api } from "./clients.js";
 export async function getTikz(type, svg, data, meta) {
   return await api.post("/convertions/tikz", { type, svg, data, meta });
 }
+
+export async function getCsv(type, data, meta) {
+  return await api.post("/convertions/csv", { type, data, meta });
+}
