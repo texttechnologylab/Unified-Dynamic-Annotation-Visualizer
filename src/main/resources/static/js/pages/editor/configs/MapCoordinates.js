85ed57d1ac82fe7a6a4c21f3746a01afbd88fae7
phpp28 <pstephan99@web.de>
2026-03-21 20:24:43 +0100

    Connect ScrollTable with API

--- src/main/resources/static/js/pages/editor/configs/MapCoordinates.js
+++ src/main/resources/static/js/pages/editor/configs/MapCoordinates.js
@@ -2,7 +2,7 @@ export default class MapCoordinates {
   static token = "MC";
   static description = `
   A generator for storing labeled and color-coded positions within a map or spatial environment.
-  <br> Compatible with: <strong>Line Chart</strong>`;
+  <br> Compatible with: <b>Line Chart, Table</b>`;
   static defaultConfig = {
     name: "New MapCoordinates",
     type: "MapCoordinates",
